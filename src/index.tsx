c085c59ce318d6b10162d82d57ffdeb56405a0b1
Naoto Kamba <naoto.kamba@outlook.jp>
2022-01-28 13:20:37 +0900

    esbuildを用いたビルド環境の構築

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+export const Index = () => {
+  return <div>Hello, World!aaa</div>;
+};
+
+ReactDOM.render(<Index />, document.getElementById("root"));
